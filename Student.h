dfc9596cbb6801fa7c933e772372106e599703f6
Thusara <99307800+it21339424@users.noreply.github.com>
2022-03-16 05:32:11 +0000

    complete

--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
 class Student {
   // private section
+private:
   //    int studentId
+  int studentId;
   //    name <- 20 charcters
-  
+  char name;
   // public section
+public:
   //    assignDetails() method declaration
+  void assignDetails();
   //    display() method declaration
+  void display();
 };
